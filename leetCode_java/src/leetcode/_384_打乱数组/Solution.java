a747e2a7fb4f8db8a50a2b03e38d5c19ae96ba33
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2021-11-22 17:59:03 +0800

    Create Solution.java

---
+++ leetCode_java/src/leetcode/_384_打乱数组/Solution.java
@@ -0,0 +1,26 @@
+package leetcode._384_打乱数组;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    private static ArrayList<int []> shuffles=new ArrayList<>();
+
+
+    public Solution(int[] nums) {
+//        shuffles.add(nums);
+    }
+    private void createShuffle(int [] nums,boolean []used){
+        
+    }
+
+
+    public int[] reset() {
+        return null;
+    }
+
+    public int[] shuffle() {
+        return null;
+    }
+}
