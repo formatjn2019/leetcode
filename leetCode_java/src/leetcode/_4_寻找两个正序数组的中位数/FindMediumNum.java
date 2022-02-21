72ba47bc035e1209872a883ce01cb2b236e759ba
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2022-02-21 14:23:34 +0800

    第四题更正

--- leetCode_java/src/leetcode/_4_寻找两个正序数组的中位数/FindMediumNum.java
+++ leetCode_java/src/leetcode/_4_寻找两个正序数组的中位数/FindMediumNum.java
@@ -4,29 +4,57 @@ public class FindMediumNum {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         //求两个数组的总长度
         int length = nums1.length + nums2.length;
-        //结果数组，方便数组总长度为偶数时求平均值
-        int []result=new int[2];
-        //遍历两个数组，总计遍历总长一半+1的长度
-        for (int n1p=0,n2p=0,count=0;count<length/2+1;count++){
-            //当其中一个数组遍历完成，防止指针越界
-            if (n1p >= nums1.length){
-                result[count%2]=nums2[n2p++];
-                continue;
+        //已经排除数的数量
+        int count = 0;
+        int l1 = 0, r1 = nums1.length, l2 = 0, r2 = nums2.length;
+        int expect = length % 2 == 1 ? length / 2 : length / 2 - 1;
+        while (count < expect) {
+            //数组1用完了
+            if (l1 == r1) {
+                l2 += expect - count;
+                break;
+            } else if (l2 == r2) {
+                l1 += expect - count;
+                break;
             }
-            if (n2p >= nums2.length){
-                result[count%2]=nums1[n1p++];
-                continue;
+            int step = Math.max(1,(expect - count) / 2);
+            int t1 = Math.min(l1 + step-1, r1 - 1), t2 = Math.min(l2 + step-1, r2 - 1);
+            if (nums1[t1] < nums2[t2]) {
+                count += t1 - l1 + 1;
+                l1 = t1 + 1;
+            } else {
+                count += t2 - l2 + 1;
+                l2 = t2 + 1;
             }
-            result[count%2]= nums1[n1p] < nums2[n2p] ? nums1[n1p++]:nums2[n2p++];
+        }
+
+        double result=0;
+        if (l1<r1){
+            result=l2<r2?nums1[l1]<nums2[l2]?nums1[l1++]:nums2[l2++]:nums1[l1++];
+        }else {
+            result=nums2[l2++];
         }
         //根据总长判定返回平均值还是中位数
-        return length%2 ==0 ? (result[1]+result[0]) / 2.0 :result[(length/2)%2];
+        if (length%2==0){
+            if (l1<r1){
+                result+=l2<r2? Math.min(nums1[l1], nums2[l2]) :nums1[l1];
+            }else {
+                result+=nums2[l2];
+            }
+            result/=2;
+        }
+        return result;
     }
+
     public static void main(String[] args) {
-        FindMediumNum findMediumNum= new FindMediumNum();
-        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,3},new int[]{2}));
-        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,2},new int[]{3,4}));
-        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{0,0},new int[]{0,0}));
-        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{},new int[]{1}));
+        FindMediumNum findMediumNum = new FindMediumNum();
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{4}, new int[]{1, 2, 3, 5}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1, 3, 4, 9}, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,4,4,5}, new int[]{1,5,5,6,8}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,2}, new int[]{1,2,3}));
+
     }
 }
---
+++ leetCode_java/src/leetcode/_4_寻找两个正序数组的中位数/FindMediumNum_count.java
@@ -0,0 +1,32 @@
+package leetcode._4_寻找两个正序数组的中位数;
+
+public class FindMediumNum_count {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //求两个数组的总长度
+        int length = nums1.length + nums2.length;
+        //结果数组，方便数组总长度为偶数时求平均值
+        int []result=new int[2];
+        //遍历两个数组，总计遍历总长一半+1的长度
+        for (int n1p=0,n2p=0,count=0;count<length/2+1;count++){
+            //当其中一个数组遍历完成，防止指针越界
+            if (n1p >= nums1.length){
+                result[count%2]=nums2[n2p++];
+                continue;
+            }
+            if (n2p >= nums2.length){
+                result[count%2]=nums1[n1p++];
+                continue;
+            }
+            result[count%2]= nums1[n1p] < nums2[n2p] ? nums1[n1p++]:nums2[n2p++];
+        }
+        //根据总长判定返回平均值还是中位数
+        return length%2 ==0 ? (result[1]+result[0]) / 2.0 :result[(length/2)%2];
+    }
+    public static void main(String[] args) {
+        FindMediumNum_count findMediumNum= new FindMediumNum_count();
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,3},new int[]{2}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{1,2},new int[]{3,4}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{0,0},new int[]{0,0}));
+        System.out.println(findMediumNum.findMedianSortedArrays(new int[]{},new int[]{1}));
+    }
+}
