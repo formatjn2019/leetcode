700455b8e74fd769e5f80c8b836462c75220f580
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2022-03-17 10:24:48 +0800

    3.17打卡

---
+++ leetCode_go/src/test/0720_词典中最长的单词_test.go
@@ -0,0 +1,11 @@
+package test
+
+import (
+	"src/problems"
+	"testing"
+)
+
+func TestLongestWord(t *testing.T){
+	//println(problems.LongestWord([]string{"a", "banana", "app", "appl", "ap", "apply", "apple"}))
+	println(problems.LongestWord([]string{"m","mo","moc","moch","mocha","l","la","lat","latt","latte","c","ca","cat"}))
+}
