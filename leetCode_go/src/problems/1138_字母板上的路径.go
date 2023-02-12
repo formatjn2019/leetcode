0143482e09cc2104a68ca40724fb1535f44fb156
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2023-02-12 12:32:36 +0800

    Update 1138_字母板上的路径.go

--- leetCode_go/src/problems/1138_字母板上的路径.go
+++ leetCode_go/src/problems/1138_字母板上的路径.go
@@ -16,18 +16,22 @@ func alphabetBoardPath(target string) string {
 	prei, prej := 0, 0
 	for _, c := range target {
 		ci, cj := int((c-'a')/5), int((c-'a')%5)
-		//优先垂直走
 		if dir := prei < ci; dir {
+			//向下移动，先左右再下
+			if prej < cj {
+				buffer.Write([]byte(strings.Repeat("R", cj-prej)))
+			} else {
+				buffer.Write([]byte(strings.Repeat("L", prej-cj)))
+			}
 			buffer.Write([]byte(strings.Repeat("D", ci-prei)))
 		} else {
 			//向上移动，先上再左右
 			buffer.Write([]byte(strings.Repeat("U", prei-ci)))
-		}
-		//向下移动，先左右再下
-		if prej < cj {
-			buffer.Write([]byte(strings.Repeat("R", cj-prej)))
-		} else {
-			buffer.Write([]byte(strings.Repeat("L", prej-cj)))
+			if prej < cj {
+				buffer.Write([]byte(strings.Repeat("R", cj-prej)))
+			} else {
+				buffer.Write([]byte(strings.Repeat("L", prej-cj)))
+			}
 		}
 		buffer.WriteRune('!')
 		prei, prej = ci, cj
