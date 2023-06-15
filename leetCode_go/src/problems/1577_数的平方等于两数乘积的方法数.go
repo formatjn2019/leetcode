c384deceff02ba46b492b066ba1448e537ef78da
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2023-06-15 14:31:53 +0800

    6.15打卡

--- leetCode_go/src/problems/1577_数的平方等于两数乘积的方法数.go
+++ leetCode_go/src/problems/1577_数的平方等于两数乘积的方法数.go
@@ -1,32 +1,32 @@
 package problems
 
 func NumTriplets(nums1 []int, nums2 []int) int {
-	return numTriplets(nums1,nums2)
+	return numTriplets(nums1, nums2)
 }
 func numTriplets(nums1 []int, nums2 []int) int {
-	hashTab1,hashTab2:=map[int64]int{},map[int64]int{}
-	for _,num:=range nums1{
-		pre,_:=hashTab1[int64(num*num)]
-		hashTab1[int64(num*num)]=pre+1
+	hashTab1, hashTab2 := map[int64]int{}, map[int64]int{}
+	for _, num := range nums1 {
+		pre, _ := hashTab1[int64(num*num)]
+		hashTab1[int64(num*num)] = pre + 1
 	}
-	for _,num:=range nums2{
-		pre,_:=hashTab2[int64(num*num)]
-		hashTab2[int64(num*num)]=pre+1
+	for _, num := range nums2 {
+		pre, _ := hashTab2[int64(num*num)]
+		hashTab2[int64(num*num)] = pre + 1
 	}
-	result:=0
-	result+=count(nums1,hashTab2)
-	result+=count(nums2,hashTab1)
+	result := 0
+	result += countNums(nums1, hashTab2)
+	result += countNums(nums2, hashTab1)
 	return result
 }
-func count(nums []int,hashTab map[int64]int) (result int) {
+func countNums(nums []int, hashTab map[int64]int) (result int) {
 	//fmt.Println(hashTab)
 	//fmt.Println(nums)
 	//arr判定
-	lenth:=len(nums)
-	for i:=0;i<lenth;i++{
-		for j:=i+1;j<lenth;j++{
-			if count,ok:=hashTab[int64(nums[i]*nums[j])];ok {
-				result+=count
+	lenth := len(nums)
+	for i := 0; i < lenth; i++ {
+		for j := i + 1; j < lenth; j++ {
+			if count, ok := hashTab[int64(nums[i]*nums[j])]; ok {
+				result += count
 			}
 		}
 	}
