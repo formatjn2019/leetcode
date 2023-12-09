b4a4b3c0f071d304c0549d58ad303cee4ac96deb
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2023-12-10 01:12:49 +0800

    Update 0070_爬楼梯.go

--- leetCode_go/src/problems/0070_爬楼梯.go
+++ leetCode_go/src/problems/0070_爬楼梯.go
@@ -8,13 +8,9 @@ func climbStairs_timeout(n int) int {
 	}
 }
 func climbStairs(n int) int {
-	if n < 3 {
-		return n
-	}
 	resultArr := []int{1, 2, 0}
-
 	for p := 2; p < n; p++ {
 		resultArr[p%3] = resultArr[(p+1)%3] + resultArr[(p+2)%3]
 	}
-	return resultArr[n%3]
+	return resultArr[(n-1)%3]
 }
